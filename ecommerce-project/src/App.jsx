7b78db42c5240a1cccbf45fe1b6a6a7ec846774c
Farah <hamlilifarah.2005@gmail.com>
2026-09-23 17:04:22 +0000

    Move checkout page into React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/checkout" element={<div>test</div>}></Route>
+        <Route index element={<HomePage />} />
+        <Route path="/checkout" element={<div>test</div>} />
       </Routes>
     </>
   );
